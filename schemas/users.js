0c7edd781d6c856b8cfc5351f863f417bfbc1d6a
RZorkin <zorkin81@gmail.com>
2023-08-13 20:59:38 +0300

    Add project files

---
+++ schemas/users.js
@@ -0,0 +1,16 @@
+const Joi = require("joi");
+
+const registerSchema = Joi.object({
+	password: Joi.string().min(8).required(),
+	email: Joi.string().required(),
+});
+
+const loginSchema = Joi.object({
+	password: Joi.string().min(8).required(),
+	email: Joi.string().required(),
+});
+
+module.exports = {
+	registerSchema,
+	loginSchema,
+};
