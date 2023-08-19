d4ac4e458ae2f0195f276a2234145fcfaee32801
RZorkin <zorkin81@gmail.com>
2023-08-19 13:48:58 +0300

    Add project files

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,12 +1,37 @@
-const sgMail = require("@sendgrid/mail");
+const nodemailer = require("nodemailer");
 require("dotenv").config();
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const { META_EMAIL, META_PASSWORD } = process.env;
 
-const sendEmail = async data => {
-	const email = { ...data, from: "vikahany@gmail.com" };
-	await sgMail.send(email);
+const nodemailerConfig = {
+	host: "smtp.meta.ua",
+	port: 465,
+	secure: true,
+	auth: {
+		user: META_EMAIL,
+		pass: META_PASSWORD,
+	},
+};
+
+const transport = nodemailer.createTransport(nodemailerConfig);
+
+const sendMail = async data => {
+	const mail = { ...data, from: META_EMAIL };
+	await transport.sendMail(mail);
 	return true;
 };
 
-module.exports = sendEmail;
+module.exports = sendMail;
+
+// const sgMail = require("@sendgrid/mail");
+// require("dotenv").config();
+
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+// const sendEmail = async data => {
+// 	const email = { ...data, from: " " };
+// 	await sgMail.send(email);
+// 	return true;
+// };
+
+// module.exports = sendEmail;
