46490ba0108f8600beef97d491850071fa00eb37
RZorkin <zorkin81@gmail.com>
2023-08-20 18:51:13 +0300

    Fix project files

--- controllers/users/resendVerifyEmail.js
+++ controllers/users/resendVerifyEmail.js
@@ -28,43 +28,18 @@
 
 // module.exports = resendVerifyEmail;
 
-// const { User } = require("../../models");
-// const { sendEmail } = require("../../helpers");
-// const { HttpError } = require("../../helpers");
-
-// const resendVerifyEmail = async (req, res) => {
-// 	const { email } = req.body;
-// 	const user = await User.findOne({ email });
-// 	if (!user) {
-// 		throw HttpError(401, "Email or password is wrong");
-// 	}
-// 	if (user.verify) {
-// 		throw HttpError(400, "Verification has already been passed");
-// 	}
-// 	const verifyEmail = {
-// 		to: email,
-// 		subject: "Verify Email",
-// 		html: `<a target="_blank" href="http://localhost:3000/users/verify/${user.verificationToken}">Verify your email</a>`,
-// 	};
-
-// 	await sendEmail(verifyEmail);
-
-// 	res.status(200).json({ message: "Verification email sent" });
-// };
-
-// module.exports = resendVerifyEmail;
-
 const { User } = require("../../models");
 const { sendEmail } = require("../../helpers");
+const { HttpError } = require("../../helpers");
 
 const resendVerifyEmail = async (req, res) => {
 	const { email } = req.body;
 	const user = await User.findOne({ email });
 	if (!user) {
-		return res.status(401).json({ message: "Email or password is wrong" });
+		throw HttpError(401, "Email or password is wrong");
 	}
 	if (user.verify) {
-		return res.status(400).json({ message: "Verification has already been passed" });
+		throw HttpError(400, "Verification has already been passed");
 	}
 	const verifyEmail = {
 		to: email,
